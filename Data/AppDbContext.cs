07557abf0c1f7ba908144b9cb50239ae448cb625
GuiSanchez1910 <guilherme.sanchez1910@gmail.com>
2025-05-01 13:49:53 -0300

    Criação seeder

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using CineTurbo.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,8 +5,42 @@ namespace CineTurbo.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions options) : base(options){}
+        public AppDbContext(DbContextOptions options) : base(options) {}
 
         public DbSet<Filme> CineTurboDB { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Filme>().HasData(
+                new Filme
+                {
+                    Id = 1,
+                    Nome = "Matrix",
+                    Sinopse = "Um hacker descobre a verdade sobre sua realidade.",
+                    AvaliacaoImdb = 87,
+                    AvaliacaoTomates = 88,
+                    Foto = "https://www.themoviedb.org/t/p/w1280/9e3Dz7aCANy5aRUQF745IlNloJ1.jpg",
+                    Genero = "Ficção Científica",
+                    Avaliacao = 5,
+                    Duracao = "2h 16min",
+                    Lancamento = new DateTime(1999, 3, 31)
+                },
+                new Filme
+                {
+                    Id = 2,
+                    Nome = "O Senhor dos Anéis: A Sociedade do Anel",
+                    Sinopse = "Um hobbit embarca em uma jornada épica para destruir um anel maligno.",
+                    AvaliacaoImdb = 88,
+                    AvaliacaoTomates = 91,
+                    Foto = "https://www.themoviedb.org/t/p/w1280/9e3Dz7aCANy5aRUQF745IlNloJ1.jpg",
+                    Genero = "Fantasia",
+                    Avaliacao = 5,
+                    Duracao = "2h 58min",
+                    Lancamento = new DateTime(2001, 12, 19)
+                }
+            );
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -28,4 +28,12 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+// using (var scope = app.Services.CreateScope())
+// {
+//     var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+//     context.Database.EnsureDeleted();  // Apaga o banco
+//     context.Database.EnsureCreated();  // Cria do zero com os dados de seed
+// }
+
+
 app.Run();
