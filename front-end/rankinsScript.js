9e301c3430446de8ff9bfb7610026437591169a6
GuiSanchez1910 <guilherme.sanchez1910@gmail.com>
2025-05-20 18:10:05 -0300

    DOOM SLAYER

---
+++ front-end/rankinsScript.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:5018/api/filmes",
+        dataType: "json",
+        success: function (response) {
+
+            response.sort((a, b) => b.avaliacao - a.avaliacao);
+
+            let html = ''
+
+            response.forEach(filme => {
+                html +=`
+                        <div>
+                            <img data-id="${filme.id}" width=300 src="${filme.foto}" alt="${filme.nome}">
+                        </div>
+                    `;
+            });
+
+            $('#filmes-container').append(html);
+        }
+    });
+});
